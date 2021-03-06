543dc3afa21e4fc737b83cc6e9d1678f0b8c4071
Antonio Manuel Perez Lopez <aperezl@gmail.com>
2021-03-06 12:46:57 +0100

    add onSubmit validator to BasicForm

--- first-project/src/components/BasicForm.js
+++ first-project/src/components/BasicForm.js
@@ -3,11 +3,23 @@ import React, { useState } from 'react'
 const BasicForm = () => {
   const [firstName, setFirstName] = useState('')
   const [lastName, setLastName] = useState('')
+  const validate = event => {
+    event.preventDefault()
+    if (!firstName.trim()) {
+      console.log('firstName is empty')
+      return
+    }
+    if(!lastName.trim()) {
+      console.log('lastName is empty')
+      return
+    }
+    console.log('valid form')
+  }
   return (
     <>
-      <form>
-        <input onChange={(e) => {setFirstName(e.target.value)}} placeholder="Firstname" type="text" />
-        <input onChange={(e) => {setLastName(e.target.value)}} placeholder="Lastname" type="text" />
+      <form onSubmit={validate}>
+        <input onChange={ e => {setFirstName(e.target.value)}} placeholder="Firstname" type="text" />
+        <input onChange={ e => {setLastName(e.target.value)}} placeholder="Lastname" type="text" />
         <input type="submit" />
       </form>
     </>
