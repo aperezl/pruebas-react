2246b2d92feec342e1111ed09c8d04ecb5b20870
Antonio Manuel Perez Lopez <aperezl@gmail.com>
2021-03-08 21:22:51 +0100

    finish User component

--- api-example/src/components/User.js
+++ api-example/src/components/User.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
+import { Link, useParams } from 'react-router-dom'
+
 
 const User = () => {
+  const [user, setUser] = useState({})
+  const { id } = useParams()
+  const getUser = async () => {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+    const user = await response.json()
+    setUser(user)
+  }
+
+  useEffect(() => {
+    getUser()
+  }, [])
+
   return (
     <div>
       <h2>User</h2>
+      <p>Name: {user.name}</p>
+      <p>Email: {user.email}</p>
+      <small>
+        Phone: {user.phone}
+      </small>
     </div>
   )
 }
