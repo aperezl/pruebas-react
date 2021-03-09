9cd9f3dba3dcff49abd1d3ebcbbfe17b7b1c3cc0
Antonio Manuel Perez Lopez <aperezl@gmail.com>
2021-03-10 00:48:40 +0100

    add logout button

--- firebase-auth/src/components/Menu.js
+++ firebase-auth/src/components/Menu.js
@@ -1,24 +1,53 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { Auth } from "../firebase.config";
+import { Link } from "react-router-dom";
 
 const Menu = () => {
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    Auth.onAuthStateChanged((user) => {
+      if (user) {
+        setUser(user.email);
+      }
+    });
+  }, []);
+  const logout = async () => {
+    await Auth.signOut();
+    setUser(null);
+  };
   return (
     <nav className="flex bg-white flex-wrap items-center justify-between p-4 shadow">
       <div className="navbar-menu hidden lg:order-1 lg:block w-full lg:w-2/5">
-          <Link to="/" className="block lg:inline-block mt-4 lg:mt-0 mr-10 text-blue-900 hover:text-indigo-600" href="#">
-              Home
-          </Link>
-          <Link to="/admin" className="block lg:inline-block mt-4 lg:mt-0 mr-10 text-blue-900 hover:text-indigo-600" href="#">
-              Admin
-          </Link>
+        <Link
+          to="/"
+          className="block lg:inline-block mt-4 lg:mt-0 mr-10 text-blue-900 hover:text-indigo-600"
+          href="#"
+        >
+          Home
+        </Link>
+        <Link
+          to="/admin"
+          className="block lg:inline-block mt-4 lg:mt-0 mr-10 text-blue-900 hover:text-indigo-600"
+          href="#"
+        >
+          Admin
+        </Link>
       </div>
       <div className="navbar-menu hidden lg:order-3 lg:block w-full lg:w-2/5 lg:text-right">
-          <Link to="/login" className="block lg:inline-block mt-4 lg:mt-0 mr-10 text-blue-900 hover:text-indigo-600" href="#">
-              Login
+        {user ? (
+          <button onClick={logout}>Logout</button>
+        ) : (
+          <Link
+            to="/login"
+            className="block lg:inline-block mt-4 lg:mt-0 mr-10 text-blue-900 hover:text-indigo-600"
+            href="#"
+          >
+            Login
           </Link>
+        )}
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Menu
+export default Menu;
