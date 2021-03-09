fe3b32016ed21487284211dfca6d647f35af2fad
Antonio Manuel Perez Lopez <aperezl@gmail.com>
2021-03-09 19:09:45 +0100

    Creater firsts components

--- firebase-auth/src/App.js
+++ firebase-auth/src/App.js
@@ -1,8 +1,13 @@
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom'
 function App() {
   return (
-    <>
+    <div className="container">
       start
-    </>
+    </div>
   );
 }
 
