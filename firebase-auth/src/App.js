d9af4c264c8d4deb26a6e9c2d902a5afedfbe9e1
Antonio Manuel Perez Lopez <aperezl@gmail.com>
2021-03-09 20:00:28 +0100

    add menu component to App

--- firebase-auth/src/App.js
+++ firebase-auth/src/App.js
@@ -3,11 +3,28 @@ import {
   Route,
   Switch
 } from 'react-router-dom'
+
+import Home from './components/Home'
+import Admin from './components/Admin'
+import Login from './components/Login'
+import Menu from './components/Menu'
+
 function App() {
   return (
-    <div className="container">
-      start
-    </div>
+    <>
+      
+      
+      <Router>
+        <Menu />
+        <div className="container mx-auto px-4">  
+          <Switch>
+            <Route exact path="/" component={Home}></Route>
+            <Route path="/admin" component={Admin}></Route>
+            <Route path="/login" component={Login}></Route>
+          </Switch>
+        </div>
+      </Router>
+    </>
   );
 }
 
--- firebase-auth/src/components/Menu.js
+++ firebase-auth/src/components/Menu.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Menu = () => {
   return (
-    <div>
-      <h2>Menu</h2>
-    </div>
+    <nav className="flex bg-white flex-wrap items-center justify-between p-4 shadow">
+      <div className="navbar-menu hidden lg:order-1 lg:block w-full lg:w-2/5">
+          <Link to="/" className="block lg:inline-block mt-4 lg:mt-0 mr-10 text-blue-900 hover:text-indigo-600" href="#">
+              Home
+          </Link>
+          <Link to="/admin" className="block lg:inline-block mt-4 lg:mt-0 mr-10 text-blue-900 hover:text-indigo-600" href="#">
+              Admin
+          </Link>
+      </div>
+      <div className="navbar-menu hidden lg:order-3 lg:block w-full lg:w-2/5 lg:text-right">
+          <Link to="/login" className="block lg:inline-block mt-4 lg:mt-0 mr-10 text-blue-900 hover:text-indigo-600" href="#">
+              Login
+          </Link>
+      </div>
+    </nav>
   )
 }
 
