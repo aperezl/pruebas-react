06ff46ef7a738bb37813dcc3565df739826cb17b
Antonio Manuel Perez Lopez <aperezl@gmail.com>
2021-03-11 19:21:30 +0100

    add error control

--- firebase-firestore/src/App.js
+++ firebase-firestore/src/App.js
@@ -1,24 +1,47 @@
+import React, { useState } from 'react'
 import { Button, ChakraProvider, Container, Box, FormControl, FormLabel, Heading, Input, Flex } from "@chakra-ui/react"
 
 function App() {
+  const [name, setName] = useState('')
+  const [phone, setPhone] = useState('')
+  const [user, setUser] = useState([])
+  const [error, setError] = useState(null)
+  
+  const setUsers = e => {
+    console.log('comprobaciones')
+    e.preventDefault()
+    
+    if (!name && !name.trim()) {
+      setError('Field name is empty')
+      return
+    }
+    if (!phone && !phone.trim()) {
+      setError('Field phone is empty')
+      return
+    }
+
+
+
+  }
   return (
     <ChakraProvider>
       <Container maxW="container.lg">
         <Flex>
           <Box w="50%">
-            <form>
+            <form onSubmit={setUsers}>
               <Heading size="lg" paddingBottom="10" as="h1">User's form</Heading>
               <FormControl id="name">
                 <FormLabel>Name</FormLabel>
-                <Input type="text"/>
+                <Input onChange={e => setName(e.target.value)} type="text"/>
               </FormControl>
               <FormControl id="phone">
                 <FormLabel>Telephone number</FormLabel>
-                <Input type="text"/>
+                <Input onChange={e => setPhone(e.target.value)} type="text"/>
               </FormControl>
               <FormControl paddingY="4">
-              <Button width="100%" colorScheme="blue">Register</Button>
+              <Button type="submit" width="100%" colorScheme="blue">Register</Button>
               </FormControl>
+              { error ? (<div>{error}</div>) : (<span></span>) }
             </form>
           </Box>
           <Box w="50%">
