abfffeffa218838edb8fbb4013a5f306f05d49d6
Antonio Manuel Perez Lopez <aperezl@gmail.com>
2021-03-11 18:31:48 +0100

    add firestore config

---
+++ firebase-firestore/src/firebase.config.js
@@ -0,0 +1,19 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDxi4IFS5-uAV9HAQQ5gHQeuhze4TON8q8",
+  authDomain: "pruebas-react-a175a.firebaseapp.com",
+  projectId: "pruebas-react-a175a",
+  storageBucket: "pruebas-react-a175a.appspot.com",
+  messagingSenderId: "677921284020",
+  appId: "1:677921284020:web:d7b632aa0e20d60b345ce1",
+  measurementId: "G-79XZDZFC9G",
+};
+// Initialize Firebase
+const fireApp = firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+const Auth = fireApp.auth();
+
+export { Auth };
